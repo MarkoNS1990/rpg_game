524f6ff32534225c314fc1ba3fe22b0cebd57ef3
Marko Zecevic <marczecevic@gmail.com>
2021-06-09 12:50:01 +0200

    Add menus and get them to show on UI scene

--- src/Scenes/UIScene.js
+++ src/Scenes/UIScene.js
@@ -1,5 +1,7 @@
 import Phaser from 'phaser';
-
+import HeroesMenu from '../Menus/HeroesMenu';
+import EnemiesMenu from '../Menus/EnemiesMenu';
+import ActionsMenu from '../Menus/ActionsMenu';
 export default class UIScene extends Phaser.Scene {
   constructor() {
     super('UIScene');
@@ -16,5 +18,35 @@ export default class UIScene extends Phaser.Scene {
     this.graphics.fillRect(95, 150, 90, 100);
     this.graphics.strokeRect(188, 150, 130, 100);
     this.graphics.fillRect(188, 150, 130, 100);
+
+    // basic container to hold all menus
+    this.menus = this.add.container();
+                
+    this.heroesMenu = new HeroesMenu(195, 153, this);           
+    this.actionsMenu = new ActionsMenu(100, 153, this);            
+    this.enemiesMenu = new EnemiesMenu(8, 153, this);   
+    
+    // the currently selected menu 
+    this.currentMenu = this.actionsMenu;
+    
+    // add menus to the container
+    this.menus.add(this.heroesMenu);
+    this.menus.add(this.actionsMenu);
+    this.menus.add(this.enemiesMenu);
+
+    this.battleScene = this.scene.get('BattleScene');
+
+    this.remapHeroes();
+    this.remapEnemies();
+    
   }
+
+    remapHeroes(){
+        const heroes = this.battleScene.heroes;
+        this.heroesMenu.remap(heroes);
+    }
+    remapEnemies(){
+        const enemies = this.battleScene.enemies;
+        this.enemiesMenu.remap(enemies);
+}
 }
